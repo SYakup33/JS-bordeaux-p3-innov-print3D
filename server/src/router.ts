0f70704a5d9cc80973a2747ab0bf945af9fcd121
ThomasCH19 <thomas.chategnier@yahoo.com>
2025-06-11 12:10:03 +0200

    added route /products

--- server/src/router.ts
+++ server/src/router.ts
@@ -2,4 +2,8 @@ import express from "express";
 
 const router = express.Router();
 
+import productListActions from "./modules/productList/productListActions";
+
+router.get("/products", productListActions.browse);
+
 export default router;
