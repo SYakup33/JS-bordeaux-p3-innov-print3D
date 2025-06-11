303a1fced988390f5f8f86b4237b36217e3b6590
ThomasCH19 <thomas.chategnier@yahoo.com>
2025-06-11 15:02:41 +0200

    added routes /images and /categories

--- server/src/router.ts
+++ server/src/router.ts
@@ -3,7 +3,12 @@ import express from "express";
 const router = express.Router();
 
 import productListActions from "./modules/productList/productListActions";
-
 router.get("/products", productListActions.browse);
+
+import imageActions from "./modules/image/imageActions";
+router.get("/images", imageActions.browse);
+
+import categoryActions from "./modules/category/categoryActions";
+router.get("/categories", categoryActions.browse);
 
 export default router;
