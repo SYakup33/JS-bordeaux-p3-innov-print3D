d3fecb07504d74e06aff80afb93e25f3e1612116
ThomasCH19 <thomas.chategnier@yahoo.com>
2025-06-17 15:24:04 +0200

    Added Home page and created bouton to navigate to /products

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,8 +1,13 @@
 import { createBrowserRouter } from "react-router";
+import Home from "./pages/Home.tsx";
 
 import Products from "./pages/ProductList.tsx";
 
 const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
   {
     path: "/products",
     element: <Products />,
