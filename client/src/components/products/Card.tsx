ef865b7351f8b88a5476bb5780064c067b8548c0
ThomasCH19 <thomas.chategnier@yahoo.com>
2025-06-17 14:36:04 +0200

    Biome

--- client/src/components/products/Card.tsx
+++ client/src/components/products/Card.tsx
@@ -2,9 +2,9 @@ import { useState } from "react";
 import "../../assets/styles/_variables.css";
 import Pagination from "rc-pagination";
 import "rc-pagination/assets/index.css";
+import btn_ajouter_panier from "../../../public/img/icons/btn_ajouter_panier.png";
+import coeur_favori_inactif from "../../../public/img/icons/coeur_favori_inactif.png";
 import type { CardProps } from "../../types/ClientType.ts";
-import btn_ajouter_panier from "../../../public/img/icons/btn_ajouter_panier.png"
-import coeur_favori_inactif from "../../../public/img/icons/coeur_favori_inactif.png"
 
 function Card({ products }: CardProps) {
   const [currentPage, setCurrentPage] = useState(1);
