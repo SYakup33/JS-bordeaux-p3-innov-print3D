66bc5d1c08326cc0c340e141317fdb550f47517e
Morelle <cynthia.morelle.cm@gmail.com>
2025-06-19 09:59:23 +0200

    created routes side backend with /api/cart and /api/cart/user/:userId

--- server/src/router.ts
+++ server/src/router.ts
@@ -2,4 +2,10 @@ import express from "express";
 
 const router = express.Router();
 
+import cartActions from "./modules/cart/cartActions";
+
+router.get("/api/cart", cartActions.browse);
+router.post("/api/cart", cartActions.add);
+router.get("/api/cart/user/:userId", cartActions.browseCartWithProducts);
+
 export default router;
