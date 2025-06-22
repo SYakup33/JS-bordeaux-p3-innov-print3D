e480260ba077f05ff65dd0865e0c5b108d9b87d6
BICHON <imane.bichon@icloud.com>
2025-06-22 18:12:30 +0200

    fix check Github : update cart.ts with biome formatter

--- client/src/types/cart.ts
+++ client/src/types/cart.ts
@@ -1,19 +1,19 @@
 type CartProduct = {
-    productId: number;
-    productName: string;
-    description: string;
-    price: number;
-    categoryName: string;
-    quantity: number;
-    images: string[];
-  };
+  productId: number;
+  productName: string;
+  description: string;
+  price: number;
+  categoryName: string;
+  quantity: number;
+  images: string[];
+};
 
-  type CartProps = {
-    products: CartProduct[];
-    selectedProducts: number[];
-    checkProduct: (productId: number) => void;
-    updateQuantity: (productId: number, newQuantity: number) => void;
-    deleteProduct: (productId: number) => void;
-    checkAll: () => void;
-    uncheckAll: () => void;
-  };
+type CartProps = {
+  products: CartProduct[];
+  selectedProducts: number[];
+  checkProduct: (productId: number) => void;
+  updateQuantity: (productId: number, newQuantity: number) => void;
+  deleteProduct: (productId: number) => void;
+  checkAll: () => void;
+  uncheckAll: () => void;
+};
