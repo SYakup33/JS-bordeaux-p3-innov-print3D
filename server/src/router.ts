e8ce5fc95a8d4e0811233b295f5dec67fba5d3c1
ThomasCH19 <thomas.chategnier@yahoo.com>
2025-06-27 11:09:19 +0200

    added route /api/products/:id

--- server/src/router.ts
+++ server/src/router.ts
@@ -4,5 +4,6 @@ const router = express.Router();
 
 import productActions from "./modules/product/productActions";
 router.get("/api/products", productActions.browse);
+router.get("/api/products/:id", productActions.read);
 
 export default router;
