6b134966d93dadda824e17d0a659b2469402b4c3
Yakup <yakup.senel33@gmail.com>
2025-07-03 11:45:43 +0200

    added new type for product filters props

--- client/src/types/product.ts
+++ client/src/types/product.ts
@@ -6,3 +6,16 @@ export type ProductType = {
   category_id: number;
   images: string[];
 };
+
+export type ProductsFilterProps = {
+  productName: string | null;
+  productNameChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  suggestions: ProductType[];
+  minPrice: number | null;
+  minPriceChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  maxPrice: number | null;
+  maxPriceChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  category: string | null;
+  categoryChange: (value: string) => void;
+  setOrder: (order: string) => void;
+};
