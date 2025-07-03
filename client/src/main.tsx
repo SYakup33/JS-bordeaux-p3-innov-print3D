568115880bd2f0195f911b27cad8b84b7ff4f61d
BICHON <imane.bichon@icloud.com>
2025-07-03 13:01:44 +0200

    Front : wrap app with UserProvider and CartProvider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,8 @@
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { CartProvider } from "./context/CartContext";
+import { UserProvider } from "./context/UserContext";
 import router from "./router";
 
 const rootElement = document.getElementById("root");
@@ -8,4 +10,10 @@ if (rootElement == null) {
   throw new Error(`Your HTML Document should contain a <div id="root"></div>`);
 }
 
-createRoot(rootElement).render(<RouterProvider router={router} />);
+createRoot(rootElement).render(
+  <UserProvider>
+    <CartProvider>
+      <RouterProvider router={router} />
+    </CartProvider>
+  </UserProvider>,
+);
