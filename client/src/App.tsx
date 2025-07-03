beb4975b1988250acdf8a036edc71734215ed055
BICHON <imane.bichon@icloud.com>
2025-07-03 12:59:28 +0200

    Front : Add Header component and onfigure toastContainer

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,24 @@
 import "./App.css";
 import "./../src/assets/styles/_variables.css";
+import { Outlet } from "react-router";
+import { ToastContainer } from "react-toastify";
+import Header from "./components/Header";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <ToastContainer
+        position="bottom-left"
+        autoClose={1000}
+        newestOnTop
+        closeOnClick
+      />
+    </>
+  );
 }
 
 export default App;
