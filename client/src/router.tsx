bd2559a0d020ca369f018c76ebd3d766b5b6d91c
BICHON <imane.bichon@icloud.com>
2025-07-09 14:48:44 +0200

    font : add login and logout routes

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -3,6 +3,8 @@ import App from "./App.tsx";
 import ConfirmedOrder from "./pages/ConfirmedOrder.tsx";
 import Home from "./pages/Home.tsx";
 import ProductList from "./pages/ProductList/ProductList.tsx";
+import Login from "./pages/auth/Login.tsx";
+import Logout from "./pages/auth/Logout.tsx";
 import CartList from "./pages/cart/CartList.tsx";
 import Product from "./pages/product/Product.tsx";
 
@@ -19,6 +21,14 @@ const router = createBrowserRouter([
       {
         path: "product/:id",
         element: <Product />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "logout",
+        element: <Logout />,
       },
       {
         path: "cart/:id",
