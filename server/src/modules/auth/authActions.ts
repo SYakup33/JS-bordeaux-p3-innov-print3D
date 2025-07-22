310cb76dd228c023de0714f738271d2231704ff3
BICHON <imane.bichon@icloud.com>
2025-07-22 12:17:41 +0200

    update trendProducts

--- server/src/modules/auth/authActions.ts
+++ server/src/modules/auth/authActions.ts
@@ -74,9 +74,6 @@ const login: RequestHandler = async (req, res, next) => {
 const verifyToken: RequestHandler = (req, res, next) => {
   try {
     const authorizationHeader = req.get("Authorization");
-    // console.log(authorizationHeader);
-
-    // console.log("==> [verifyToken] PATH:", req.path);
 
     if (authorizationHeader == null) {
       throw new Error("Token manquant");
