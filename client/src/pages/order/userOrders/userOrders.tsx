dc45bdc531c373d699330bf36130fee24b65b605
Morelle <cynthia.morelle.cm@gmail.com>
2025-07-23 10:40:21 +0200

    style(userOrder) : unified with style of adminOrders displaying in front

--- client/src/pages/order/userOrders/userOrders.tsx
+++ client/src/pages/order/userOrders/userOrders.tsx
@@ -6,6 +6,7 @@ import type { UserOrder } from "../../../types/order";
 
 const UserOrders = () => {
   const [userOrders, setUserOrders] = useState<UserOrder[]>([]);
+  const [showDetails, setShowDetails] = useState<number[]>([]);
   const { token, currentUser } = useAuth();
   const userId = currentUser?.id;
 
@@ -25,6 +26,16 @@ const UserOrders = () => {
     };
     fetchUserOrders();
   }, [token, userId]);
+
+  const btnShowDetails = (orderId: number) => {
+    setShowDetails((orderIds) => {
+      if (!orderIds.includes(orderId)) {
+        return [...orderIds, orderId];
+      }
+      orderIds.includes(orderId);
+      return orderIds.filter((id) => id !== orderId);
+    });
+  };
 
   const orderRecap = userOrders.map((userOrder) => ({
     userOrder,
@@ -55,7 +66,7 @@ const UserOrders = () => {
               >
                 <div className="border rounded bg-gradient p-4 mb-4 shadow-lg">
                   <div className="row gy-4">
-                    <div className="col-12 col-md-5 d-flex flex-column">
+                    <div className="ccol-12 col-md-12 d-flex flex-column">
                       <h4 className="text-dark mb-3 d-flex align-items-center">
                         <span>Commande #{userOrder.orderId}</span>
                       </h4>
@@ -66,12 +77,12 @@ const UserOrders = () => {
                           {totalArticles === 1 ? "article" : "articles"}
                         </span>
 
-                        <span className="border rounded-pill bg-info text-dark fw-bold d-flex align-items-center gap-1 px-4 py-2 shadow-sm">
+                        <span className="border rounded-pill text-dark fw-bold d-flex align-items-center gap-1 px-4 py-2 shadow-sm">
                           {totalPrice.toFixed(2)} €
                         </span>
 
                         <div className="d-flex align-items-center gap-2">
-                          <span className="fw-semibold text-dark">
+                          <span className="fw-semibold text-success">
                             Statut : {userOrder.status}
                           </span>
                         </div>
@@ -79,38 +90,51 @@ const UserOrders = () => {
                     </div>
                   </div>
                   <div className="d-flex flex-column flex-md-row text-md-center w-100 text-muted mb-3 small">
-                    <div className="col-md-4 mb-2 ">
+                    <div className="col-md-4 pt-3 mb-2 text-center ">
                       <strong>Date :</strong>
                       {new Date(userOrder.createdAt).toLocaleDateString()}
                     </div>
                   </div>
-                  <div className="border-top mt-2">
-                    <div className="order-products-scroll">
-                      <ul className="row list-unstyled g-3 mt-1">
-                        {userOrder.products.map((p) => (
-                          <li
-                            key={p.productId}
-                            className="col-6 col-md-4 col-lg-3 text-center"
-                          >
-                            <div className="bg-white p-3 rounded shadow-sm h-100">
-                              <img
-                                src={p.image}
-                                alt={p.productName}
-                                className="img-fluid rounded mb-2 admin-order-product-img"
-                              />
-                              <div className="fw-medium small">
-                                <span className="text-dark">{p.quantity}</span>{" "}
-                                x{" "}
-                                <span className="text-dark">
-                                  {p.productName}
-                                </span>
+                  <button
+                    type="button"
+                    onClick={() => btnShowDetails(userOrder.orderId)}
+                    className="fw-semibold px-2 py-1 bg-transparent text-danger border-0"
+                  >
+                    {showDetails.includes(userOrder.orderId)
+                      ? "Masquer les détails"
+                      : "Afficher les détails"}
+                  </button>
+                  {showDetails.includes(userOrder.orderId) && (
+                    <div className="border-top mt-2">
+                      <div className="order-products-scroll">
+                        <ul className="row list-unstyled g-3 mt-1">
+                          {userOrder.products.map((p) => (
+                            <li
+                              key={p.productId}
+                              className="col-6 col-md-4 col-lg-3 text-center"
+                            >
+                              <div className="bg-white p-3 rounded shadow-sm h-100">
+                                <img
+                                  src={p.image}
+                                  alt={p.productName}
+                                  className="img-fluid rounded mb-2 admin-order-product-img"
+                                />
+                                <div className="fw-medium small">
+                                  <span className="text-dark">
+                                    {p.quantity}
+                                  </span>{" "}
+                                  x{" "}
+                                  <span className="text-dark">
+                                    {p.productName}
+                                  </span>
+                                </div>
                               </div>
-                            </div>
-                          </li>
-                        ))}
-                      </ul>
+                            </li>
+                          ))}
+                        </ul>
+                      </div>
                     </div>
-                  </div>
+                  )}
                 </div>
               </article>
             ))}
