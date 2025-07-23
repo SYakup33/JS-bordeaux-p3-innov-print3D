0978f06b104d6406fda28e9e34eb0e94dcd962c4
Morelle <cynthia.morelle.cm@gmail.com>
2025-07-23 12:24:37 +0200

    feat(paymentfail) : in client added a navigate on click of the button " return to the list of products" direction products

--- client/src/pages/Payment Issue/PaymentFail.tsx
+++ client/src/pages/Payment Issue/PaymentFail.tsx
@@ -1,7 +1,9 @@
 import { BagX, Receipt } from "react-bootstrap-icons";
+import { useNavigate } from "react-router";
 import logo from "/img/icons/lnnovPrintLogo.png";
 
 function PaymentFail() {
+  const navigate = useNavigate();
   return (
     <>
       <div className="d-flex align-items-center justify-content-start p-3 p-md-5 cart-header-title mb-4">
@@ -23,8 +25,9 @@ function PaymentFail() {
             <button
               type="button"
               className=" btn w-100 btn-lg fw-semibold confirmed-order-btn-animated"
+              onClick={() => navigate("/products")}
             >
-              Retourner à mon panier
+              Retourner à la liste des produits
             </button>
           </div>
         </article>
