6f3feb37cd4a7723e1fc4923ab534ff96393c3af
Yakup <yakup.senel33@gmail.com>
2025-07-24 12:11:00 +0200

    added customer review

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,13 +1,29 @@
+import { Star, StarFill, StarHalf } from "react-bootstrap-icons";
 import { useNavigate } from "react-router";
 import pokeball_arcanin_1 from "../../../img/pokeball_arcanin_1.jpg";
 import CustomDesign from "../../../public/img/icons/CustomDesign.jpg";
 import FastTruck from "../../../public/img/icons/FastTruck.jpg";
+import GoogleLogo from "../../../public/img/icons/GoogleLogo.png";
 import ResponsiblePurchase from "../../../public/img/icons/ResponsiblePurchase.jpg";
 import TrendProducts from "../../components/product/trendProducts/TrendProducts";
 import "./Home.css";
 
 function Home() {
   const navigate = useNavigate();
+  const renderStars = (note: number) => {
+    const stars = [];
+    for (let i = 1; i <= 5; i++) {
+      const key = `star-${note}-${i}`;
+      if (i <= Math.floor(note)) {
+        stars.push(<StarFill key={key} className="home-stars" size={15} />);
+      } else if (i - 0.5 === note) {
+        stars.push(<StarHalf key={key} className="home-stars" size={15} />);
+      } else {
+        stars.push(<Star key={key} className="home-stars" size={15} />);
+      }
+    }
+    return stars;
+  };
   return (
     <>
       <section className="home-browse-creation text-black d-md-flex">
@@ -43,7 +59,7 @@ function Home() {
             <div className="card">
               <img
                 src={FastTruck}
-                className="test card-img-top w-25 mx-auto pt-1"
+                className="card-img-top w-25 mx-auto pt-1"
                 alt="Camion rapide"
               />
               <div className="card-body">
@@ -83,6 +99,71 @@ function Home() {
                 <p className="card-text home-choose-us-p">
                   Fabriqué à base de filament PLA composé de polymère et
                   d'amidon de maïs.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <h2 className="text-center fw-semibold mt-5">Les avis de nos clients</h2>
+      <section className="home-choose-us d-flex justify-content-center">
+        <div className="container row w-75 text-center">
+          <div className="col-md-4 mt-2">
+            <div className="card">
+              <div className="p-2">
+                <div className="d-flex align-items-center w-75 justify-content-end ms-4">
+                  <h3 className="fw-bold fs-6 mt-3 w-75">Eric Dupont</h3>
+                  <img
+                    src={GoogleLogo}
+                    className="review-google-img pt-1 ms-1"
+                    alt="Google"
+                  />
+                </div>
+                <p className="mb-1 fw-lighter">18/02/2025</p>
+                <div className="">{renderStars(5)}</div>
+                <p className="card-text fs-6">
+                  Génial ! Merci, produits top, vendeur top, efficace et prix
+                  correct !
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4 mt-2">
+            <div className="card">
+              <div className="p-2">
+                <div className="d-flex align-items-center w-75 justify-content-end ms-4">
+                  <h3 className="fw-bold fs-6 mt-3 w-75">Camille Duret</h3>
+                  <img
+                    src={GoogleLogo}
+                    className="review-google-img pt-1 ms-1"
+                    alt="Google"
+                  />
+                </div>
+                <p className="mb-1 fw-lighter">07/03/2025</p>
+                <div className="">{renderStars(3)}</div>
+                <p className="card-text fs-6">
+                  La qualité est correct. En attente de nouveaux produits. Pour
+                  le reste parfait.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4 mt-2">
+            <div className="card">
+              <div className="p-2">
+                <div className="d-flex align-items-center w-75 justify-content-end ms-4">
+                  <h3 className="fw-bold fs-6 mt-3 w-75">Sylvie Durand</h3>
+                  <img
+                    src={GoogleLogo}
+                    className="review-google-img pt-1 ms-1"
+                    alt="Google"
+                  />
+                </div>
+                <p className="mb-1 fw-lighter">11/05/2025</p>
+                <div className="">{renderStars(4.5)}</div>
+                <p className="card-text fs-6">
+                  Produit correspondant à la photo. Rapide et efficace, je
+                  recommande vivement
                 </p>
               </div>
             </div>
